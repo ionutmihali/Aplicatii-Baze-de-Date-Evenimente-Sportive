ffb33b7590492ca02779e94c977d0c1980c4b75d
Ionut Mihali <107608894+ionutmihali@users.noreply.github.com>
2022-11-30 16:48:11 +0200

    V1

---
+++ TemaABD/TemaABD/CUtilizator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaABD
+{
+    internal class CUtilizator
+    {
+        protected string username { get; set; }
+        protected string password { get; set; }
+    }
+}
