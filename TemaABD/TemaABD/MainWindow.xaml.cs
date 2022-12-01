120819f023b17f8ad20e27d28349b2e8af5f5f7f
Vlad Popescu <107830910+vladpopescu123@users.noreply.github.com>
2022-12-01 15:12:51 +0200

    Update MainWindow.xaml.cs

--- TemaABD/TemaABD/MainWindow.xaml.cs
+++ TemaABD/TemaABD/MainWindow.xaml.cs
@@ -25,16 +25,68 @@ namespace TemaABD
             InitializeComponent();
         }
 
-        private void Butonadmin_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
         private void Butoncontnou_Click(object sender, RoutedEventArgs e)
         {
             CreareCont c = new CreareCont();
             this.Close();
             c.Show();
+        }
+        
+        private void Butonantrenor_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new SportsEntities())
+            {
+                var results = from c in context.Utilizatoris
+                              select new
+                              {
+                                 c.username,
+                                 c.parola
+                              };
+
+                if (user.Text == "admin")
+                {
+                    if (pass.Password == "admin")
+                    {
+                        MessageBox.Show("Eroare la autentificare");
+                        return;
+
+                    }
+                }
+
+                foreach (var item in results)
+                { 
+                    if (user.Text == item.username)
+                    {
+                        if (pass.Password == item.parola)
+                        {
+                            MessageBox.Show("Antrenor logat!");
+                            break;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Eroare la autentificare!");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        private void Butonadmin_Click(object sender, RoutedEventArgs e)
+        {
+            if (user.Text == "admin")
+            {
+                if (pass.Password == "admin")
+                {
+                    MessageBox.Show("Administrator logat!");
+
+                }
+                else
+                {
+                    MessageBox.Show("Eroare la autentificare: Not an administrator!");
+
+                }
+            }
         }
     }
 
