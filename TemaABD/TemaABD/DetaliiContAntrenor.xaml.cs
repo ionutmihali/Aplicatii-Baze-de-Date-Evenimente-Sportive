628fdc26e836cd03ff188b535956e27509efd4b0
Ionut Mihali <107608894+ionutmihali@users.noreply.github.com>
2022-12-08 21:36:21 +0200

    new new

---
+++ TemaABD/TemaABD/DetaliiContAntrenor.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TemaABD
+{
+    /// <summary>
+    /// Interaction logic for DetaliiContAntrenor.xaml
+    /// </summary>
+    public partial class DetaliiContAntrenor : Window
+    {
+        private string sp = "Alege sport";
+        public DetaliiContAntrenor()
+        {
+            InitializeComponent();
+            string text ="";
+            var a = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            if (a != null)
+            {
+                text = a.user.Text;
+            }
+            
+            text1.Text = text;
+            text2.Text = sp;
+
+            LoadCombobox();
+        }
+
+        public void LoadCombobox()
+        {
+
+            var context = new SportsEntities();
+
+            List < Sporturi > s = context.Sporturis.ToList();
+            combo.ItemsSource = s;
+            combo.DisplayMemberPath = "denumire";
+            combo.SelectedValuePath = "IDSport";
+            combo.Text = "Select";
+
+        }
+
+        private void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            sp = combo.SelectedItem.ToString();
+        }
+    }
+}
---
+++ TemaABD/TemaABD/EvenimenteAntrenor.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TemaABD
+{
+    /// <summary>
+    /// Interaction logic for EvenimenteAntrenor.xaml
+    /// </summary>
+    public partial class EvenimenteAntrenor : Window
+    {
+        public EvenimenteAntrenor()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (var context = new SportsEntities())
+            {
+                var results = from s in context.EvenimentSportivIndividuals
+                              select new
+                              {
+                                  s.IdEvenimentSportivIndividual,
+                                  s.nume,
+                                  s.player1,
+                                  s.player2,
+                                  s.scor1,
+                                  s.scor2
+                              };
+
+                evenimentSportivIndividualDataGrid.ItemsSource = results.ToList();
+
+                var results1 = from s in context.EvenimentSportivIndividuals
+                               select new
+                               {
+                                   s.IdEvenimentSportivIndividual,
+                                   s.nume,
+                                   s.player1,
+                                   s.player2,
+                                   s.scor1,
+                                   s.scor2
+                               };
+
+                evenimentSportivEchipeDataGrid.ItemsSource = results1.ToList();
+            }
+            TemaABD.SportsDataSet sportsDataSet = ((TemaABD.SportsDataSet)(this.FindResource("sportsDataSet")));
+            // Load data into the table EvenimentSportivEchipe. You can modify this code as needed.
+            TemaABD.SportsDataSetTableAdapters.EvenimentSportivEchipeTableAdapter sportsDataSetEvenimentSportivEchipeTableAdapter = new TemaABD.SportsDataSetTableAdapters.EvenimentSportivEchipeTableAdapter();
+            sportsDataSetEvenimentSportivEchipeTableAdapter.Fill(sportsDataSet.EvenimentSportivEchipe);
+            System.Windows.Data.CollectionViewSource evenimentSportivEchipeViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("evenimentSportivEchipeViewSource")));
+            evenimentSportivEchipeViewSource.View.MoveCurrentToFirst();
+            // Load data into the table EvenimentSportivIndividual. You can modify this code as needed.
+            TemaABD.SportsDataSetTableAdapters.EvenimentSportivIndividualTableAdapter sportsDataSetEvenimentSportivIndividualTableAdapter = new TemaABD.SportsDataSetTableAdapters.EvenimentSportivIndividualTableAdapter();
+            sportsDataSetEvenimentSportivIndividualTableAdapter.Fill(sportsDataSet.EvenimentSportivIndividual);
+            System.Windows.Data.CollectionViewSource evenimentSportivIndividualViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("evenimentSportivIndividualViewSource")));
+            evenimentSportivIndividualViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MeniuAntrenor m = new MeniuAntrenor();
+            m.Show();
+            this.Close();
+        }
+
+        private void evenimentSportivEchipeDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+        }
+    }
+}
