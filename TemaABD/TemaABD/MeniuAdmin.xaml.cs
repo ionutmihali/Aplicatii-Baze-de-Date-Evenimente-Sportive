edff6725a5d9bcc891a331956e297eb22a8f6ab3
Vlad Popescu <107830910+vladpopescu123@users.noreply.github.com>
2022-12-08 22:43:42 +0200

    NEW

---
+++ TemaABD/TemaABD/MeniuAdmin.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TemaABD
+{
+    /// <summary>
+    /// Interaction logic for MeniuAdmin.xaml
+    /// </summary>
+    public partial class MeniuAdmin : Window
+    {
+        public MeniuAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)     //buton adaugare sport
+        {
+            AdugareSport adugareSport = new AdugareSport();
+            adugareSport.Show();
+          
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)   //buton Log Out
+        {
+            MainWindow m = new MainWindow();
+            this.Close();
+            m.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)  //buton adaugare studenti
+        {
+            AdaugareStudent addStud = new AdaugareStudent();
+            addStud.Show();
+        }
+
+    }
+}
